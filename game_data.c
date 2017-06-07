85f722f83641b61ff18fe8b8425ed31b7f56e358
Hadrien Daures <hadrien.daures@gmail.com>
2017-06-07 14:13:44 +0200

    added a more generic function to load data. loading screen and first ui item ok

--- game_data.c
+++ game_data.c
@@ -3,22 +3,48 @@
 
 struct s_game_data game_data;
 
-int data_load_loading_screen() {
+int data_load_raw(ansigraphic_image_t* dest, char*src) {
   ansigraphic_ivector2_t xy;
   int x, y;
-  int width = raw_data.loading_screen_w;
-  int height = raw_data.loading_screen_h;
-  game_data.loading_screen.image = ansigraphic_newImage(width, height);
-  game_data.loading_screen.xy.x = game.width/2 - width;
-  game_data.loading_screen.xy.y = game.height/2 - height;
+  int width = dest->width;
+  int height = dest->height;
+  char val;
+
   y = -1;
   while (++y < height) {
     x = -1;
     while (++x < width) {
       xy.x = x;
       xy.y = height - 1 - y;
-      ansigraphic_pixelSetColor(game_data.loading_screen.image, &xy, "015", "000");
-      ansigraphic_pixelSetValue(game_data.loading_screen.image, &xy, raw_data.loading_screen[x + (y*width)]);
+      val = src[x + (y*width)];
+      if (val < 0) {
+	ansigraphic_pixelSetColor(dest, &xy, "015", "000");
+	ansigraphic_pixelSetValue(dest, &xy, -val);
+      } else {
+	ansigraphic_pixelSetColor(dest, &xy, "000", "015");
+	ansigraphic_pixelSetValue(dest, &xy, val);
+      }
     }
   }
+  return 0;
+}
+
+int data_load_loading_screen() {
+  int width = raw_data.loading_screen_w;
+  int height = raw_data.loading_screen_h;
+  game_data.loading_screen.image = ansigraphic_newImage(width, height);
+  game_data.loading_screen.xy.x = game.width/2 - width;
+  game_data.loading_screen.xy.y = game.height/2 - height;
+  data_load_raw(game_data.loading_screen.image, raw_data.loading_screen);
+  return 0;
+}
+
+int data_load_ui_stats() {
+  int width = raw_data.ui_stats_w;
+  int height = raw_data.ui_stats_h;
+  game_data.ui_stats.image = ansigraphic_newImage(width, height);
+  game_data.ui_stats.xy.x = 0;
+  game_data.ui_stats.xy.y = 0;
+  data_load_raw(game_data.ui_stats.image, raw_data.ui_stats);
+  return 0;
 }
--- game_data.h
+++ game_data.h
@@ -2,5 +2,6 @@
 #define GAME_DATA_H_
 
 int data_load_loading_screen();
+int data_load_ui_stats();
 
 #endif /* !GAME_DATA_H_ */
