ea81802898af07d57a7e08357601239e827733ee
Hadrien Daures <hadrien.daures@gmail.com>
2017-06-09 15:48:33 +0200

    still bugs with save manager

--- src/saveManager.c
+++ src/saveManager.c
@@ -11,26 +11,28 @@
 char* saveFile;
 
 int saveManager_load() {
-  int fd, read_byte, size=(sizeof(game) + sizeof(dhms_clock));
+  int fd, read_byte, size=(sizeof(game_t) + sizeof(struct s_dhms_clock));
 
   if (fd = open(saveFile, O_RDONLY) <= 0)
     game.magic=0;
-  else {
-    lseek(fd, -size, SEEK_END);
-    read(fd, &game, sizeof(game));
-    read(fd, &dhms_clock, sizeof(dhms_clock));
+  else {    
+    //lseek(fd, -size, SEEK_END);
+    read(fd, &game, sizeof(game_t));
+    read(fd, &dhms_clock, sizeof(struct s_dhms_clock));
     close(fd);
+    printf("%lu\n", game.magic);
+    exit(123);
   }
   return 0;
 }
 
 int saveManager_save() {
-  int fd, read_byte, size=(sizeof(game) + sizeof(dhms_clock));
+  int fd, read_byte, size=(sizeof(game_t) + sizeof(struct s_dhms_clock));
 
   fd = open(saveFile, O_CREAT|O_WRONLY, (mode_t)0666);
-  lseek(fd, -size, SEEK_END);
-  write(fd, &game, sizeof(game));
-  write(fd, &dhms_clock, sizeof(dhms_clock));
+  //lseek(fd, -size, SEEK_END);
+  write(fd, &game, sizeof(game_t));
+  write(fd, &dhms_clock, sizeof(struct s_dhms_clock));
   close(fd);
   printf("game saved\n");
   return 0;
